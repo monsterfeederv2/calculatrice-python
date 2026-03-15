a8c6c9d99ad96c6660c3e6561c87623e3d169016
MonsterFeeder <alca63991@eleve.isep.fr>
2026-03-15 23:03:06 +0100

    affichage hello world pour tester

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import tkinter as tk
+
+root = tk.Tk()
+frm = tk.Frame(root)
+frm.grid()
+tk.Label(frm, text="Hello World!").grid(column=0, row=0)
+tk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
